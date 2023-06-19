ec14080d390c24b4c805e42204cc2613351850e8
ronit18 <ronitgandhi96@gmail.com>
2023-06-19 15:57:22 +0530

    Made Header component.

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+
+const Header = () => {
+  const { data: sessionData } = useSession();
+  return (
+    <div className="navbar bg-primary text-primary-content">
+      <div className="flex-1 pl-5 text-3xl font-bold">
+        {sessionData?.user?.name ? `Notes for ${sessionData?.user?.name}` : ""}
+      </div>
+      <div className="flex-none gap-2">
+        <div className="dropdown-end dropdown">
+          {sessionData?.user ? (
+            <label
+              onClick={() => void signOut()}
+              tabIndex={0}
+              className="btn-ghost btn-circle avatar btn"
+            >
+              <div className="w-10 rounded-full">
+                <Image
+                  src={sessionData.user?.image ?? ""}
+                  alt={`{${sessionData.user?.name ?? ""} profile image}`}
+                  className="h-3 w-3 rounded-full"
+                  width={33}
+                  height={33}
+                />
+              </div>
+            </label>
+          ) : (
+            <button
+              className="btn-ghost rounded-btn btn"
+              onClick={() => void signIn()}
+            >
+              Sign in
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
