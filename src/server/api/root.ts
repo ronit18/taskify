e901f0bcb66ad3172d184645f300bd3ee7e79b88
ronit18 <ronitgandhi96@gmail.com>
2023-06-19 16:24:03 +0530

    Added api logic to create delete and get all notes.

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { exampleRouter } from "~/server/api/routers/example";
 import { topicsRouter } from "~/server/api/routers/topics";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { noteRouter } from "./routers/note";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   topics: topicsRouter,
+  note: noteRouter,
 });
 
 // export type definition of API
